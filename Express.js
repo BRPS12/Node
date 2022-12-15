2d9a32f0545a519b5358e46a4f5bcc7f7068894f
2022HOPE-BoldErdene <2022hope-bolderdene@27s-iMac.local>
2022-12-15 19:38:54 +0800

    ig brg duusla

--- Express.js
+++ Express.js
@@ -1,14 +1,67 @@
-import express, { response } from "express";
+import express from "express";
 const port = 8000;
 const app = express();
-app.get("/", (request, response) => {
-  response.send("Hello Express Server");
+const users = [];
+const posts = [];
+app.get("/", (req, res) => {
+  res.send({
+    data: users,
+    post: posts,
+  });
 });
-app.get("/boocko", (req, res) => {
-  res.send("bocko");
+app.use(express.json());
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  users.map((users) => {
+    if (users.username === username && users.password === password) {
+      res.send({
+        success: true,
+        msg: "Amjilttai",
+      });
+    }
+  });
+  res.send({
+    success: false,
+    msg: "Amjiltgui",
+  });
 });
-app.post("/zurag", (req, res) => {
-  res.send("ZURADGGGG");
+app.post("/signup", (req, res) => {
+  const { username, password, email, phonenumber } = req.body;
+  const user = {
+    username,
+    password,
+    email,
+    phonenumber,
+  };
+  users.push(user);
+  res.send({
+    succes: true,
+    msg: "amjilttai burtguulle",
+  });
+});
+app.post("/create", (req, res) => {
+  const { img, description, title } = req.body;
+  const post = {
+    img,
+    description,
+    title,
+  };
+  posts.push(post);
+  console.log(post);
+  res.send({
+    success: true,
+    data: posts,
+  });
+});
+app.put("/", (req, res) => {
+  res.send("Put");
+});
+app.patch("/", (req, res) => {
+  res.send("Patch");
+});
+app.delete("/", (req, res) => {
+  res.send("Delete");
 });
 app.get("/api", (req, res) => {
   res.send({
